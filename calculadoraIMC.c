4574db8e1b8163fa24bff379ee194f20c3aa2d92
axaves <67404018+axaves@users.noreply.github.com>
2024-08-10 12:34:00 -0300

    Update calculadoraIMC.c

--- calculadoraIMC.c
+++ calculadoraIMC.c
@@ -18,23 +18,15 @@ int main() {
     printf("Seu IMC é: %.2f\n", imc);
 
     // Verificar a faixa do IMC
-    if (imc < 16) {
-        printf("Magreza grave.\n");
-    } else if (imc >= 16 && imc <= 16.9) {
-        printf("Magreza moderada.\n");
-    } else if (imc >= 17 && imc <= 18.5) {
-        printf("Magreza leve.\n");
-    } else if (imc >= 18.6 && imc <= 24.9) {
-        printf("Peso ideal.\n");
-    } else if (imc >= 25 && imc <= 29.9) {
+    if (imc < 18.5) {
+        printf("Magreza.\n");
+    } else if (imc >= 18.5 && imc < 24.9) {
+        printf("Normal.\n");
+    } else if (imc >= 24.9 && imc <= 30) {
         printf("Sobrepeso.\n");
-    } else if (imc >= 30 && imc <= 34.9) {
-        printf("Obesidade grau I.\n");
-    } else if (imc >= 35 && imc <= 39.9) {
-        printf("Obesidade grau II (severa).\n");
-    } else {
-        printf("Obesidade grau III (mórbida).\n");
-    }
+    } else if (imc > 30 ) {
+        printf("Obesidade.\n");
+    } 
 
     return 0;
 }
